9a848b8fbbf21b26098283f4ca7a13708348bfb8
Omkarnbh87 <contact.omkar87@gmail.com>
2024-04-24 16:59:23 +0530

    Student class added

--- Main.java
+++ Main.java
@@ -4,5 +4,7 @@ class Main{
     public static void main(String args[]){
         System.out.println("Main class main method invoked");
         System.out.println(new Date());
+        Student student = new Student();
+        student.sayHello();
     }
 }
---
+++ Student.java
@@ -0,0 +1,8 @@
+public class Student {
+    Student(){
+        System.out.println("Student is created...");
+    }
+    public void sayHello(){
+        System.out.println("Hi I am a student....");
+    }
+}
