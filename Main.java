99a5813cc50246107b07066f613ea59e95f1eceb
Omkarnbh87 <contact.omkar87@gmail.com>
2024-04-24 16:55:02 +0530

    add a line

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+import java.util.Date;
+
 class Main{
     public static void main(String args[]){
         System.out.println("Main class main method invoked");
+        System.out.println(new Date());
     }
 }
